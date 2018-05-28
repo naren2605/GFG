6bb903e9633ea03fd9debaade468b1efe677a382
naren2605 <naren26051991@gmail.com>
2018-05-28 16:50:56 +0530

    added kadane's algo

--- src/main/java/gfg/KandanesAlgorithm.java
+++ src/main/java/gfg/KandanesAlgorithm.java
@@ -11,33 +11,80 @@ public class KandanesAlgorithm {
 		int[] array=new int[10];
 		
 		Random rand=new Random();
-	
+/*	for(int k=0;k<10;k++){
 		for(int i=0;i<10;i++){
 			array[i]=rand.nextInt(10);
 			if(rand.nextBoolean()){
 				array[i]=0-array[i];
 			}
 		}
-		System.out.println(Arrays.toString(array));
+		System.out.print(Arrays.toString(array));
 		bruteForceMaxSumSubArray(array);
-		
-		
+		kandanesMaxSumSubArray(array);
+		System.out.println();
+	}*/
+		bruteForceMaxSumSubArray(new int[]{-1,16,-15,24,-23,25});
+		kandanesMaxSumSubArray(new int[]{-1,16,-15,24,-23,25});
+		kandanesMaxSumSubArray2(new int[]{-1,16,-15,24,-23,25});
 	}
 	
 	
 	public static void bruteForceMaxSumSubArray(int[] array){
 		
 		int sum=Integer.MIN_VALUE;
-		for (int i = 0; i < array.length-1; i++) {
+		for (int i = 0; i < array.length; i++) {
 			int temp=0;
 			for (int j = i; j < array.length; j++) {
-				temp=temp+array[i];
+				temp=temp+array[j];
 				if (temp>sum) sum=temp;
 			}
 			
 		}
 		
-		System.out.println(sum);
+		System.out.print("="+sum);
 	}
+	
+	/*
+	 *  order n time, order n space
+	 * 
+	 */
+			
+	public static void kandanesMaxSumSubArray(int[] array){
+		
+		int sums[]=new int[array.length];
+		sums[0]=array[0];
+		int 		sum=sums[0];
+		for (int i = 1; i < array.length; i++) {
+		
+			sums[i]=Math.max(array[i], sums[i-1]+array[i]);
+			sum=Math.max(sum, sums[i]);
+		}
+
+	System.out.print(" >>> "+sum);		
+		}
+		
+	
+
+
+
+/*
+ * order n time, order 1 space
+ */
+public static void kandanesMaxSumSubArray2(int[] array){
+	
+	int sums[]=new int[array.length];
+	sums[0]=array[0];
+	int 		sum=sums[0];
+	int sumNminus1=sums[0];
+	for (int i = 1; i < array.length; i++) {
+	
+		sumNminus1=Math.max(array[i], sumNminus1+array[i]);
+		sum=Math.max(sum, sumNminus1);
+	}
+
+System.out.print(" >>> "+sum);		
+	}
+	
+
 
 }
